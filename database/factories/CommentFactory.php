182390e7c8e42a41fbe1983f77421cf090604b21
David Walker <drw0469@gmail.com>
2018-11-12 18:10:45 -0600

    Create factory tests and modify phpunit XML to recognize 'tests/Factory' directory.

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -6,6 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
-
+        'parent_comment_id' => 0,
+        'user_comment' => 'This is a comment!'
     ];
 });
