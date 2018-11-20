269f064fe51e8ff29ce5113933b32685ea007fc0
jlug331221 <jlug331221@gmail.com>
2018-11-19 21:12:00 -0600

    Add Bulma CSS framework to the project. Ready to start master layout.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.app_master')
 
 @section('title', 'Home')
 
-@section('content')
-    <h2 class="container">This is the home page for a user.</h2>
+@section('main-content')
+{{-- Home Page --}}
 @endsection
---
+++ resources/views/layouts/app_master.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'JeepUp') }} | @yield('title')</title>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="app">
+            <header>
+                <nav></nav>
+            </header>
+            
+            <main>
+                @yield('main-content')
+            </main>
+
+            <footer>
+                This is the footer.
+            </footer>
+        </div> <!-- End of Vue application div tag -->
+    </body>
+
+</html>
