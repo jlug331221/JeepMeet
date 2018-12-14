14e8f855a2218aed4a179ae7f2d7e94d771ceea0
drw0469 <drw0469@yahoo.com>
2018-12-13 18:15:23 -0600

    Add required attribute to contact page fields.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,17 +10,32 @@
             <h2 class="title is-2">Contact Us</h2>
 
             <form action="">
-                <div class="field">
-                    <label class="label">Name</label>
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+                <div class="field is-horizontal">
+                    <div class="field-body">
+                        <div class="field required">
+                        <label class="label">First Name
+                            <span class="contact-red-asterisk">*</span>
+                        </label>
+                            <div class="control">
+                                <input class="input" type="text" placeholder="First Name" required>
+                            </div>
+                        </div>
+                
+                        <div class="field">
+                            <label class="label">Last Name</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="Last Name">
+                                </div>
+                         </div>
                     </div>
                 </div>
-
+                
                 <div class="field">
-                    <label class="label">Email</label>
+                    <label class="label">Email
+                        <span class="contact-red-asterisk">*</span>
+                    </label>
                     <div class="control has-icons-left has-icons-right">
-                        <input class="input" type="email" placeholder="Email input">
+                        <input class="input" type="email" placeholder="Email" required>
                         <span class="icon is-small is-left">
                             <i class="fas fa-envelope"></i>
                         </span>
@@ -31,9 +46,20 @@
                 </div>
 
                 <div class="field">
-                    <label class="label">Message</label>
+                    <label class="label">Message
+                        <span class="contact-red-asterisk">*</span>
+                    </label>
                     <div class="control">
-                        <textarea class="textarea" placeholder="Textarea"></textarea>
+                        <textarea class="textarea" placeholder="Type your message here" required></textarea>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button button-filled is-rounded">Submit</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-text contact-ahref">Clear</button>
                     </div>
                 </div>
             </form>
