482296caf1ded6b0421a046801caedbc5dc08d6b
jlug331221 <jlug331221@gmail.com>
2018-12-15 19:25:21 -0600

    Start of welcome page design.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,14 @@
 
 @section('main-content')
 
-{{-- Welcome Page --}}
+<div class="columns is-mobile is-centered">
+    <div class="column is-three-quarters-mobile is-half-tablet is-half-desktop">
+        <section class="section is-medium">
+            <figure class="image is-16by9">
+                <img src="{{ asset('storage/svg/jeep_military.svg') }}" alt="Military Jeep">
+            </figure>
+        </section>
+    </div>
+</div>
 
 @endsection
