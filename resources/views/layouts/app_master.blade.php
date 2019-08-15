8fb8f7f73e1301e9873402f1f5124738f48d0812
jlug331221 <jlug331221@gmail.com>
2019-08-15 19:50:20 -0400

    Change site theme. Tabs spaces are set to 2 for HTML and CSS files.

--- resources/views/layouts/app_master.blade.php
+++ resources/views/layouts/app_master.blade.php
@@ -1,107 +1,118 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'JeepMeet') }} | @yield('title')</title>
+    <title>{{ config('app.name', 'JeepMeet') }} | @yield('title')</title>
 
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
 
-    <body class="has-navbar-fixed-top">
-        <div id="app">
-            <header>
-                <nav class="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
-                    <div class="container"> 
-                        <div class="navbar-brand">
-                            <a href="/"><h1 class="brand-name navbar-item">JeepMeet</h1></a>
-                        
-                            <a role="button" class="navbar-burger burger is-active" aria-label="menu" aria-expanded="false" data-target="jeepup-navbar">
-                                <span aria-hidden="true"></span>
-                                <span class="small-burger" aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                            </a>
-                        </div>
-                    
-                        <div id="jeepup-navbar" class="navbar-menu is-active">
-                            <div class="navbar-start">
-                                @auth
-                                    <a class="navbar-item" href="{{ url('/profile') }}">
-                                        <strong>Profile</strong>
-                                    </a>
+  <body class="has-navbar-fixed-top">
+    <div id="app">
+      <header>
+        <nav class="navbar is-fixed-top is-transparent" role="navigation" 
+          aria-label="main navigation">
+          <div class="container"> 
+            <div class="navbar-brand">
+              <a href="/"><h1 class="brand-name navbar-item">JeepMeet</h1></a>
+          
+              <a role="button" class="navbar-burger burger is-active" 
+                aria-label="menu" aria-expanded="false" data-target="jeepup-navbar">
+                <span aria-hidden="true"></span>
+                <span class="small-burger" aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+          
+            <div id="jeepup-navbar" class="navbar-menu is-active">
+              <div class="navbar-start">
+                @auth
+                  <a class="navbar-item" href="{{ url('/profile') }}">
+                      <strong>Profile</strong>
+                  </a>
 
-                                <a class="navbar-item" href="{{ url('/events') }}">
-                                        <strong>Events</strong>
-                                    </a>
-                                @endauth
+                  <a class="navbar-item" href="{{ url('/events') }}">
+                    <strong>Events</strong>
+                  </a>
+                @endauth
 
-                                <a class="navbar-item" href="{{ url('/forum') }}">
-                                    <strong>Forum</strong>
-                                </a>
+                <a class="navbar-item" href="{{ url('/forum') }}">
+                  <strong>Forum</strong>
+                </a>
 
-                                <a class="navbar-item" href="{{ url('/news') }}">
-                                    <strong>News</strong>
-                                </a>
+                <a class="navbar-item" href="{{ url('/news') }}">
+                  <strong>News</strong>
+                </a>
 
-                                <a class="navbar-item" href="{{ url('/about') }}">
-                                    <strong>About</strong>
-                                </a>
+                <a class="navbar-item" href="{{ url('/about') }}">
+                  <strong>About</strong>
+                </a>
 
-                                <a class="navbar-item" href="{{ url('/contact') }}">
-                                    <strong>Contact</strong>
-                                </a>
-                            </div>
-                        
-                            <div class="navbar-end">
-                                <div class="navbar-item">
-                                    @guest
-                                        <div class="buttons">
-                                            <a class="button button-empty is-rounded" href="{{ url('/login') }}"><strong>Login</strong></a>
-                                            <a class="button button-filled is-rounded signup-button" href="{{ url('/join') }}"><strong>Join</strong></a>
-                                        </div>
-                                    @endguest
-
-                                    @auth
-                                        <div class="buttons">
-                                            <a class="buttons button-empty is-rounded" href=""><strong>Logout</strong></a>
-                                        </div>    
-                                    @endauth
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
+                <a class="navbar-item" href="{{ url('/contact') }}">
+                  <strong>Contact</strong>
+                </a>
+              </div>
             
-            <main class="main-wrapper">
-                @yield('main-content')
-            </main>
+              <div class="navbar-end">
+                <div class="navbar-item">
+                  @guest
+                    <div class="buttons">
+                      <a class="button button-empty is-rounded login-button" 
+                        href="{{ url('/login') }}"><strong>Login</strong></a>
+                      <a class="button button-filled is-rounded signup-button" 
+                        href="{{ url('/join') }}"><strong>Join</strong></a>
+                    </div>
+                  @endguest
 
-            <footer class="footer">
-                <div class="content has-text-centered">
-                    <p><small>&copy;</small>&nbsp;<span class="footer-brand">JeepMeet</span>&nbsp;2018-{{ date('Y') }}.&nbsp;All rights reserved.</p>
-                    <p><a href="#">Legal Notice</a></p>
-                    <p><a href="#">Privacy Statement</a></p>
-                    <a href="mailto:jeepmeetup@gmail.com">jeepmeetup@gmail.com</a>
+                  @auth
+                    <div class="buttons">
+                      <a class="buttons button-empty is-rounded" href="">
+                        <strong>Logout</strong>
+                      </a>
+                    </div>    
+                  @endauth
                 </div>
-            </footer>
-        </div> <!-- End of Vue application div tag -->
-    </body>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+        
+      <main class="main-wrapper">
+        @yield('main-content')
+      </main>
+
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p><small>&copy;</small>&nbsp;<span class="footer-brand">JeepMeet</span>&nbsp;
+            2018-{{ date('Y') }}.&nbsp;All rights reserved.</p>
+          <p><a href="#">Legal Notice</a></p>
+          <p><a href="#">Privacy Statement</a></p>
+          <a href="mailto:jeepmeetup@gmail.com">
+            <span class="icon">
+              <i class="far fa-envelope fa-lg envelope-contact-us"></i>
+            </span>
+          </a>
+        </div>
+      </footer>
+    </div> <!-- End of Vue application div tag -->
+  </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,103 +5,120 @@
 @section('main-content')
 
 <div class="columns is-mobile is-centered welcome-hero-background">
-    <div class="column is-three-quarters-mobile is-half-tablet is-half-desktop">
-        <section class="section welcome-military-jeep">
-            <figure class="image is-16by9">
-                <img src="{{ asset('svg/jeep_military.svg') }}" alt="Military Jeep">
-            </figure>
-        </section>
-    </div>
+  <div class="column is-three-quarters-mobile is-half-tablet is-half-desktop">
+    <section class="section welcome-military-jeep">
+      <figure class="image is-16by9">
+        <img src="{{ asset('svg/jeep_military.svg') }}" alt="Military Jeep">
+      </figure>
+    </section>
+  </div>
 </div>
 
-<section class="section is-medium">
-    <div class="columns is-mobile is-centered welcome-hero-text">
-        <div class="column is-full-mobile is-full-tablet is-full-desktop">
-            <section class="hero">
-                <div class="hero-body">
-                    <div class="container">
-                        <h1 class="title is-size-3-mobile is-size-2-tablet is-size-1-desktop">Welcome to JeepMeet</h1>
-                        <h4 class="welcome-montserrat subtitle">The One Stop Place to Meet Fellow Jeepers</h4>
-                        <em><small>
-                            <p class="welcome-montserrat welcome-small-subtitle-text">
-                                No matter the time or place, JeepMeet is here to help you find people that are just as enthusiastic and passionate about Jeeps.
-                            </p>
-                        </small></em>
-                    </div>
-                </div>
-            </section>
+<section class="section">
+  <div class="columns is-mobile is-centered welcome-hero-text">
+    <div class="column is-full-mobile is-full-tablet is-full-desktop">
+      <section class="hero">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title is-size-3-mobile is-size-2-tablet 
+              is-size-1-desktop">Welcome to JeepMeet</h1>
+
+            <h4 class="welcome-montserrat subtitle">The One Stop Place to Meet Fellow Jeepers</h4>
+
+            <em>
+              <small>
+                <p class="welcome-montserrat welcome-small-subtitle-text">
+                  No matter the time or place, JeepMeet is here to help you find people that are 
+                  just as enthusiastic and passionate about Jeeps.
+                </p>
+              </small>
+            </em>
+          </div>
         </div>
+      </section>
+    </div>
+  </div>
+
+  <div class="columns is-centered">
+    <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
+      <section class="section has-text-centered">
+        <figure class="image is-96x96 welcome-small-figures">
+          <img src="{{ asset('svg/map.svg') }}" alt="Map Location">
+        </figure>
+
+        <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet 
+          is-size-4-desktop">Schedule a JeepMeet</h3>
+      </section>
+    </div>
+  
+    <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
+      <section class="section has-text-centered">
+        <figure class="image is-96x96 welcome-small-figures">
+          <img src="{{ asset('svg/roads.svg') }}" alt="Roads Traveled via Jeeps">
+        </figure>
+
+        <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet 
+          is-size-4-desktop">Travel to Your JeepMeet</h3>
+      </section>
     </div>
 
-    <div class="columns is-centered">
-        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
-            <section class="section has-text-centered">
-                <figure class="image is-96x96 welcome-small-figures">
-                    <img src="{{ asset('svg/map.svg') }}" alt="Map Location">
-                </figure>
-    
-                <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet is-size-4-desktop">Schedule a JeepMeet</h3>
-            </section>
-        </div>
-    
-        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
-            <section class="section has-text-centered">
-                <figure class="image is-96x96 welcome-small-figures">
-                    <img src="{{ asset('svg/roads.svg') }}" alt="Roads Traveled via Jeeps">
-                </figure>
-    
-                <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet is-size-4-desktop">Travel to Your JeepMeet</h3>
-            </section>
-        </div>
-    
-        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
-            <section class="section has-text-centered">
-                <figure class="image is-96x96 welcome-small-figures">
-                    <img src="{{ asset('svg/people.svg') }}" alt="Jeepers">
-                </figure>
-    
-                <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet is-size-4-desktop">Meet Fellow Jeepers</h3>
-            </section>
-        </div>
+    <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
+      <section class="section has-text-centered">
+        <figure class="image is-96x96 welcome-small-figures">
+          <img src="{{ asset('svg/people.svg') }}" alt="Jeepers">
+        </figure>
+
+        <h3 class="welcome-h3 has-text-weight-semibold is-size-6-mobile is-size-5-tablet 
+          is-size-4-desktop">Meet Fellow Jeepers</h3>
+      </section>
     </div>
+  </div>
 </section>
 
 <section class="section is-medium welcome-whitesmoke">
-    <div class="columns is-centered">
-        <div class="column has-text-centered is-full-mobile is-one-half-tablet is-one-half-desktop">
-            <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">
-                Engage with members of the community on our <a href="#"><span class="welcome-forum-text has-text-weight-bold">Forum</span></a>.
-            </h3>
+  <div class="columns is-centered">
+    <div class="column has-text-centered is-full-mobile is-one-half-tablet is-one-half-desktop">
+      <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">
+          Engage with members of the community on our 
+          <a href="#">
+            <span class="welcome-forum-text has-text-weight-bold">Forum</span>
+          </a>.
+      </h3>
 
-            <div class="columns is-centered">
-                <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
-                    <figure class="image is-96x96 welcome-small-figures welcome-svg-margin-top">
-                        <img src="{{ asset('svg/forum.svg') }}" alt="Forum">
-                    </figure>
-                </div>
-
-                <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
-                    <figure class="image is-96x96 welcome-small-figures welcome-svg-margin-top">
-                        <img src="{{ asset('svg/online-comments.svg') }}" alt="Online Comments">
-                    </figure>
-                </div>
-            </div>
+      <div class="columns is-centered">
+        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
+          <figure class="image is-96x96 welcome-small-figures welcome-svg-margin-top">
+            <img src="{{ asset('svg/forum.svg') }}" alt="Forum">
+          </figure>
         </div>
 
-        <div class="column has-text-centered welcome-column-margin-top is-full-mobile is-one-half-tablet is-one-half-desktop">
-            @guest
-                <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">Join today and post your first JeepMeet.</h3>
-
-                <a class="button button-filled is-rounded signup-button welcome-signup-button" href="{{ url('/join') }}"><strong>Join</strong></a>
-            @endguest
-
-            @auth
-                <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">Go to your profile and build up that JeepMeet Cred!</h3>
-
-                <a class="button button-empty is-rounded signup-button welcome-profile-button" href="{{ url('/profile') }}"><strong>Profile</strong></a>
-            @endauth
+        <div class="column is-full-mobile is-one-third-tablet is-one-third-desktop">
+          <figure class="image is-96x96 welcome-small-figures welcome-svg-margin-top">
+            <img src="{{ asset('svg/online-comments.svg') }}" alt="Online Comments">
+          </figure>
         </div>
+      </div>
     </div>
+
+    <div class="column has-text-centered welcome-column-margin-top is-full-mobile 
+      is-one-half-tablet is-one-half-desktop">
+      @guest
+        <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">Join today and post your 
+          first JeepMeet.</h3>
+
+        <a class="button button-filled is-rounded signup-button welcome-signup-button" 
+          href="{{ url('/join') }}"><strong>Join</strong></a>
+      @endguest
+
+      @auth
+        <h3 class="is-size-6-mobile is-size-5-tablet is-size-4-desktop">Go to your profile and 
+          build up that JeepMeet Cred!</h3>
+
+        <a class="button button-empty is-rounded signup-button welcome-profile-button" 
+          href="{{ url('/profile') }}"><strong>Profile</strong></a>
+      @endauth
+    </div>
+  </div>
 </section>
 
 @endsection
